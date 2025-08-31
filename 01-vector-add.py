efaedbd7529cd38f56f684417323a3ea3b0c4c52
Vasudev Menon <vasudev_goku10@outlook.com>
2025-08-31 20:11:18 +0530

    Update Python version to 3.13, fixed directory structure and adjust package index settings in configuration files

--- 01-vector-add.py
+++ 01-vector-add.py
@@ -18,23 +18,25 @@ In doing so, you will learn about:
 # Compute Kernel
 # --------------
 
-import torch
+from datetime import datetime
+import os
 
+import torch
 import triton
 import triton.language as tl
-from datetime import datetime
 
 DEVICE = triton.runtime.driver.active.get_active_torch_device()
 
 
 @triton.jit
-def add_kernel(x_ptr,  # *Pointer* to first input vector.
-               y_ptr,  # *Pointer* to second input vector.
-               output_ptr,  # *Pointer* to output vector.
-               n_elements,  # Size of the vector.
-               BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
-               # NOTE: `constexpr` so it can be used as a shape value.
-               ):
+def add_kernel(
+    x_ptr,  # *Pointer* to first input vector.
+    y_ptr,  # *Pointer* to second input vector.
+    output_ptr,  # *Pointer* to output vector.
+    n_elements,  # Size of the vector.
+    BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+    # NOTE: `constexpr` so it can be used as a shape value.
+):
     # There are multiple 'programs' processing different data. We identify which program
     # we are here:
     pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
@@ -68,7 +70,7 @@ def add(x: torch.Tensor, y: torch.Tensor):
     # The SPMD launch grid denotes the number of kernel instances that run in parallel.
     # It is analogous to CUDA launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
     # In this case, we use a 1D grid where the size is the number of blocks:
-    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )  # noqa: E731
+    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)  # noqa: E731
     # NOTE:
     #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
     #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
@@ -90,8 +92,7 @@ output_torch = x + y
 output_triton = add(x, y)
 print(output_torch)
 print(output_triton)
-print(f'The maximum difference between torch and triton is '
-      f'{torch.max(torch.abs(output_torch - output_triton))}')
+print(f"The maximum difference between torch and triton is {torch.max(torch.abs(output_torch - output_triton))}")
 
 # %%
 # Seems like we're good to go!
@@ -107,24 +108,25 @@ print(f'The maximum difference between torch and triton is '
 
 @triton.testing.perf_report(
     triton.testing.Benchmark(
-        x_names=['size'],  # Argument names to use as an x-axis for the plot.
+        x_names=["size"],  # Argument names to use as an x-axis for the plot.
         x_vals=[2**i for i in range(12, 28, 1)],  # Different possible values for `x_name`.
         x_log=True,  # x axis is logarithmic.
-        line_arg='provider',  # Argument name whose value corresponds to a different line in the plot.
-        line_vals=['triton', 'torch'],  # Possible values for `line_arg`.
-        line_names=['Triton', 'Torch'],  # Label name for the lines.
-        styles=[('blue', '-'), ('green', '-')],  # Line styles.
-        ylabel='GB/s',  # Label name for the y-axis.
-        plot_name='vector-add-performance',  # Name for the plot. Used also as a file name for saving the plot.
+        line_arg="provider",  # Argument name whose value corresponds to a different line in the plot.
+        line_vals=["triton", "torch"],  # Possible values for `line_arg`.
+        line_names=["Triton", "Torch"],  # Label name for the lines.
+        styles=[("blue", "-"), ("green", "-")],  # Line styles.
+        ylabel="GB/s",  # Label name for the y-axis.
+        plot_name="vector-add-performance",  # Name for the plot. Used also as a file name for saving the plot.
         args={},  # Values for function arguments not in `x_names` and `y_name`.
-    ))
+    )
+)
 def benchmark(size, provider):
     x = torch.rand(size, device=DEVICE, dtype=torch.float32)
     y = torch.rand(size, device=DEVICE, dtype=torch.float32)
     quantiles = [0.5, 0.2, 0.8]
-    if provider == 'torch':
+    if provider == "torch":
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: x + y, quantiles=quantiles)
-    if provider == 'triton':
+    if provider == "triton":
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: add(x, y), quantiles=quantiles)
     gbps = lambda ms: 3 * x.numel() * x.element_size() * 1e-9 / (ms * 1e-3)  # noqa: E731
     return gbps(ms), gbps(max_ms), gbps(min_ms)
@@ -134,4 +136,7 @@ def benchmark(size, provider):
 # We can now run the decorated function above. Pass `print_data=True` to see the performance number, `show_plots=True` to plot them, and/or
 # `save_path='/path/to/results/' to save them to disk along with raw CSV data:
 # manually save the plot image because for some reason it saves empty images
-benchmark.run(print_data=True, show_plots=True, save_path=f"results/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
+
+results_dir = os.path.join("results", datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+os.makedirs(results_dir, exist_ok=True)
+benchmark.run(print_data=True, show_plots=True, save_path=results_dir)
